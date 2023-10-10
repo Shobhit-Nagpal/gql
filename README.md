968c2571ea530ce07338fc7f790bd55f435289fa
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-10-10 20:46:48 +0530

    Changed title and did some basic API fetching

---
+++ README.md
@@ -0,0 +1,3 @@
+## About
+This repo is just a small project to get familiar with GraphQL and also try out a lil of that nice tailwind, you know what I mean.
+Basically, uses GraphQL queries to display SpaceX info and style with Tailwind.
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const query = `query {
+    countries {
+        code
+    }
+}`;
+
+fetch("https://countries.trevorblades.com/graphql", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+        query: query
+    }) 
+}).then(res => res.json())
+.then(data => console.log(data))
