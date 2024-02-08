04b2f381964c2a85a063e479e65a4bcaa1598795
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-02-08 14:18:58 +0530

    Refactored some stuff

--- src/index.js
+++ src/index.js
@@ -11,82 +11,78 @@ const initialQuery = `query {
 }`;
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetchData(initialQuery);
-    const continentData = response.data.continents; 
+  const response = await fetchData(initialQuery);
+  const continentData = response.data.continents;
 
-    for (let i = 0 ; i < continentData.length ; i++) {
-        const continent = document.createElement("option");
-        continent.value = continentData[i].code;
-        continent.innerText = continentData[i].name;
-        continents.appendChild(continent);
-    }
+  for (let i = 0; i < continentData.length; i++) {
+    const continent = document.createElement("option");
+    continent.value = continentData[i].code;
+    continent.innerText = continentData[i].name;
+    continents.appendChild(continent);
+  }
 });
 
 continents.addEventListener("change", async (e) => {
+  continentInfo.innerHTML = "";
+  const code = e.target.value;
 
-    continentInfo.innerHTML = "";
-    const code = e.target.value;
+  if (code === "nil") {
+    continentInfo.innerHTML = `<p class="text-center my-3">Please select a continent to show countries</p>`;
+    return;
+  }
 
-    if (code === "nil") { 
-        continentInfo.innerHTML = `<p class="text-center my-3">Please select a continent to show countries</p>`;
-       return;
+  const data = await getCountriesData(code);
+  const countriesData = data.data.continent.countries;
+
+  const infoHeading = document.createElement("h2");
+  infoHeading.innerText = "Countries";
+  infoHeading.classList.add("mx-10");
+  infoHeading.classList.add("text-3xl");
+  infoHeading.classList.add("font-medium");
+  infoHeading.classList.add("my-3");
+  continentInfo.appendChild(infoHeading);
+
+  const countriesList = document.createElement("ol");
+
+  for (let i = 0; i < countriesData.length; i++) {
+    const country = document.createElement("li");
+    country.classList.add("mx-20");
+    country.classList.add("text-xl");
+    country.classList.add("list-decimal");
+
+    const countryInfo = document.createElement("div");
+    const countryName = document.createElement("h3");
+    const countryLangs = document.createElement("p");
+
+    countryInfo.classList.add("my-4");
+
+    countryName.classList.add("font-medium");
+    countryName.classList.add("text-xl");
+
+    const langs = [];
+
+    if (countriesData[i].languages.length === 0) {
+      langs.push("English");
+    } else {
+      for (let j = 0; j < countriesData[i].languages.length; j++) {
+        langs.push(countriesData[i].languages[j].name);
+      }
     }
 
-    const data = await getCountriesData(code);
-    const countriesData = data.data.continent.countries;
-    console.log(countriesData);
+    countryName.innerText = countriesData[i].name;
+    countryLangs.innerText = `The languages spoken in this country are ${langs.join(", ")}`;
 
+    countryInfo.appendChild(countryName);
+    countryInfo.appendChild(countryLangs);
+    country.appendChild(countryInfo);
+    countriesList.appendChild(country);
+  }
 
-    const infoHeading = document.createElement("h2"); 
-    infoHeading.innerText = "Countries";
-    infoHeading.classList.add("mx-10");
-    infoHeading.classList.add("text-3xl");
-    infoHeading.classList.add("font-medium");
-    infoHeading.classList.add("my-3");
-    continentInfo.appendChild(infoHeading);
-
-    const countriesList = document.createElement("ol"); 
-
-    for (let i = 0 ; i < countriesData.length ; i++) {
-        const country = document.createElement("li");
-        country.classList.add("mx-20");
-        country.classList.add("text-xl");
-        country.classList.add("list-decimal");
-
-        const countryInfo = document.createElement("div");
-        const countryName = document.createElement("h3");
-        const countryLangs = document.createElement("p");
-
-        countryInfo.classList.add("my-4");
-
-        countryName.classList.add("font-medium");
-        countryName.classList.add("text-xl");
-
-        const langs = [];
-
-        if (countriesData[i].languages.length === 0) {
-            langs.push("English");
-        } else {
-
-            for (let j = 0 ; j < countriesData[i].languages.length ; j++) {
-                langs.push(countriesData[i].languages[j].name);
-            }
-        }
-
-        countryName.innerText = countriesData[i].name;
-        countryLangs.innerText = `The languages spoken in this country are ${langs.join(", ")}`;
-
-        countryInfo.appendChild(countryName);
-        countryInfo.appendChild(countryLangs); 
-        country.appendChild(countryInfo);
-        countriesList.appendChild(country);
-    }
-
-    continentInfo.appendChild(countriesList);
+  continentInfo.appendChild(countriesList);
 });
 
-async function getCountriesData(countryCode) {
-    const countriesQuery = `query getCountriesData($code: ID!) {
+async function getCountriesData(continentCode) {
+  const countriesQuery = `query ($code: ID!) {
         continent(code: $code) {
             name
             countries {
@@ -98,21 +94,21 @@ async function getCountriesData(countryCode) {
         }
     }`;
 
-    const data = await fetchData(countriesQuery, { code: countryCode });
-    return data;
+  const data = await fetchData(countriesQuery, { code: continentCode });
+  return data;
 }
+
 async function fetchData(query, variables) {
-    const response = await fetch("https://countries.trevorblades.com/graphql", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            query: query,
-            variables: variables
-        })
-    });
+  const response = await fetch("https://countries.trevorblades.com/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: query,
+      variables: variables,
+    }),
+  });
 
-    const data = await response.json();
+  const data = await response.json();
 
-    return data;
+  return data;
 }
-
